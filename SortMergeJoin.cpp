06f6a326e10a73d3337938cf0ae304e4b2a32c1d
Alexis Bayle <alexis.bayle@etu.u-bordeaux.fr>
2016-04-18 11:48:41 +0900

    error find with merge (duplicate results)

--- SortMergeJoin.cpp
+++ SortMergeJoin.cpp
@@ -35,46 +35,46 @@ namespace SMJ{
 
     void mergeRelations(int* startR, int* endR, int* startS, int* endS,
                         vector<string>& results, uint rowR, uint rowS){
-        uint row_R = rowR, row_S = rowS;
+        int *tupleR = startR, *tupleS = startS;
 
         // loop until there is no tuples to read in both relation
-        while(startR != endR && startS != endS){
-            if(*startR > *startS){
+        while(tupleR != endR && tupleS != endS){
+            if(*tupleR > *tupleS){
                 // move to a greater tuple in S
-                startS++;
-                row_S++;
-            } else if(*startR < *startS){
+                tupleS++;
+                rowS++;
+            } else if(*tupleR < *tupleS){
                 // move to a greater tuple in R
-                startR++;
-                row_R++;
+                tupleR++;
+                rowR++;
             } else{
                 // two equal tuples found -> record the rows
-                results.push_back(getTuple(row_R, startR, row_S, startS));
+                results.push_back(getTuple(rowR, tupleR, rowS, tupleS));
 
                 // loop on s to find other equal tuples after
-                auto tupleS2 = startS + 1;
-                auto rowS2 = row_S + 1;
+                auto tupleS2 = tupleS + 1;
+                auto rowS2 = rowS + 1;
 
-                while(tupleS2 != endS && *tupleS2 == *startR){
-                    results.push_back(getTuple(row_R, startR, rowS2, tupleS2));
+                while(tupleS2 != endS && *tupleS2 == *tupleR){
+                    results.push_back(getTuple(rowR, tupleR, rowS2, tupleS2));
                     tupleS2++;
                 }
 
                 // loop on r to find other equal tuples after
-                auto tupleR2 = startR + 1;
-                auto rowR2 = row_R + 1;
+                auto tupleR2 = tupleR + 1;
+                auto rowR2 = rowR + 1;
 
-                while(tupleR2 != endR && *tupleR2 == *startS){
-                    results.push_back(getTuple(rowR2, tupleR2, row_S, startS));
+                while(tupleR2 != endR && *tupleR2 == *tupleS){
+                    results.push_back(getTuple(rowR2, tupleR2, rowS, tupleS));
                     tupleR2++;
                 }
 
                 // go to higher tuples
-                startR++;
-                row_R++;
+                tupleR++;
+                rowR++;
 
-                startS++;
-                row_S++;
+                tupleS++;
+                rowS++;
             }
         }
     }
--- SortMergeJoin.h
+++ SortMergeJoin.h
@@ -6,9 +6,9 @@
 namespace SMJ{
 
     // constant
-    const int R_SIZE = 10; // number of rows in R
-    const int S_SIZE = 10; // number of rows in S
-    const int MAX_RAND_SMJ = 20; // max value for random integer
+    const int R_SIZE = 20; // number of rows in R
+    const int S_SIZE = 20; // number of rows in S
+    const int MAX_RAND_SMJ = 14; // max value for random integer
 
     /*
      * launch a join and display result
